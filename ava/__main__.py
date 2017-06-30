ad1cba4e05046980dbf2462ffc3bc630cb5597b3
Dorian Amouroux <dor.amouroux@gmail.com>
2017-06-30 10:06:42 +0200

    boilerplate daemon

---
+++ ava/__main__.py
@@ -0,0 +1,3 @@
+from .ava import main
+
+main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import ava
+
+setup(
+    name='ava',
+    version=ava.__version__,
+    packages=find_packages(),
+    author='AVA Project',
+    author_email='ava_2018@labeip.epitech.eu',
+    description='The daemon of the AVA Project',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'flask==0.12',
+        'requests',
+    ],
+    include_package_data=True,
+    url='https://github.com/ava-project/ava-core',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'avad = ava.ava:main',
+        ],
+    },
+)
