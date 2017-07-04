c140f1272dfcc9392ce27e0cd94e1ae2d6e0652a
David Tran <Opcoz@localhost.localdomain>
2017-07-05 07:59:09 +0900

    [Watson][LiveStreamAlternative] 	- Recording voice and transformation into audio file 	- File sent and decoded

--- ava/watson/speechtotextlivefile.py
+++ ava/watson/speechtotextlivefile.py
@@ -1,13 +1,66 @@
-import json
+import json, pyaudio, wave, base64, threading
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 
-speech_to_text = SpeechToTextV1(
-    username='b189f5ce-1f20-4dd9-a97d-0b49b9bd2318',
-    password='cUHVuqPzAOvP',
-    x_watson_learning_opt_out=False
-)
 
-print(json.dumps(speech_to_text.models(), indent=2))
+class SpeechToTextFile():
 
-print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
+    def __init__(self):
+        self.stt = SpeechToTextV1(
+            username = '9d526cf4-63de-47da-be7d-e5662d3cd1a9',
+            password = 'oWaCHHSTzjkO',
+            x_watson_learning_opt_out=False
+        )
+        self.listening = True
+
+    def listen(self):
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=16000,
+                        input=True,
+                        frames_per_buffer=2048)
+
+        self.all_datas = []
+        print ("Recording.. Press enter to finish")
+        while self.listening:
+            data = stream.read(2048)
+            self.all_datas.append(data)
+
+        stream.stop_stream()
+        stream.close()
+        self.writeToFile(p)
+        p.terminate()
+
+    def writeToFile(self, p):
+        wf = wave.open("sample.wav", "wb")
+        wf.setnchannels(1)
+        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(16000)
+        wf.writeframes(b''.join(self.all_datas))
+        wf.close()
+        self.sendFile()
+
+    def sendFile(self):
+        with open('sample.wav', 'rb') as audio_file:
+            print(json.dumps(self.stt.recognize(
+                audio_file, content_type='audio/wav', timestamps=True,
+                word_confidence=True),
+                             indent=2))
+
+
+    def close(self):
+        self.listening = False
+
+def main():
+    try:
+        stt_client = SpeechToTextFile()
+        stt_client.reading_thread = threading.Thread(target=stt_client.listen)
+        stt_client.reading_thread.start()
+        input()
+    finally:
+        stt_client.close()
+
+if __name__ == "__main__":
+    main()
