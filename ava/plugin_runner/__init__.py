8e4526af9f65820d6da9d7afbfb476095b67a2ca
Tommy <thomas.milox@epitech.eu>
2017-07-06 01:49:56 +0200

    need to fix the communication through the pipe

--- ava/plugin_runner/__init__.py
+++ ava/plugin_runner/__init__.py
@@ -18,14 +18,15 @@ class PluginRunner(_BaseComponent):
         command = self.queue_plugin_command.get()
         print('Plugin runner execute : {}'.format(command))
         command = command.split(' ')
-        print(command)
-        if self.store.get_plugin(command[0]) is not None:
+        if self.store.get_plugin(command[0]):
             if self.store.is_plugin_disabled(command[0]):
                 self.queue_tts.put('The plugin ' + command[0] + ' is currently disabled.')
             else:
                 process = self.store.get_plugin_process(command[0])
+                # TODO fix
                 process.stdin.write(str.encode(str(' '.join(command[1:]))))
-                self.queue_tts.put(process.stdout.read())
+                print(process.stdout.read())
+                # self.queue_tts.put(process.stdout.read())
         else:
             self.queue_tts.put('No plugin named ' + command[0] + ' found.')
         self.queue_plugin_command.task_done()
--- ava/process/python_plugin_process.py
+++ ava/process/python_plugin_process.py
@@ -1,35 +1,48 @@
-import os, sys, json
+import os, sys, json, io
+# TODO check
 from importlib.machinery import SourceFileLoader
 
 PLUGIN = {}
 
 def main():
+    """
+    """
     name = sys.argv[1]
     import_module(name)
     while True:
-        cmd = sys.stdin.read()
+        # TODO fix
+        cmd = sys.stdin.buffer.read()
         execute(name, cmd)
 
 def import_module(name):
+    """
+    """
     path = os.path.join(os.path.expanduser("~"), ".ava", "plugins", name)
     with open(os.path.join(path, "manifest.json")) as json_file:
-        data = json.load(json_file)
-    if PLUGIN.get(name) is None:
-        if 'build' in data:
+        manifest = json.load(json_file)
+    if not PLUGIN.get(name):
+        if 'build' in manifest:
             install_from_requirements(path)
-        mod = SourceFileLoader(name, os.path.join(path, data['source'])).load_module()
+        # TODO check
+        mod = SourceFileLoader(name, os.path.join(path, manifest['source'])).load_module()
         PLUGIN[name] = getattr(sys.modules[mod.__name__], name)
 
 def install_from_requirements(path):
+    """
+    """
     requirements = os.path.join(path, "requirements.txt")
     pip.main(['install', '-r', requirements])
 
 def execute(name, command):
-    if PLUGIN.get(name) is not None:
-        self_ = PLUGIN[name]
-        print('name = ', name, ' command = ', command)
-    else:
-        print('An error occurred.')
+    """
+    """
+    if PLUGIN.get(name):
+        cmd = command.split(' ')
+        plugin = PLUGIN.get(name)
+        if cmd[0] in plugin.__dict__:
+            print(plugin.__dict__[cmd[0]](plugin, str(' '.join(cmd[1:])) if len(cmd) > 1 else ''))
+            return
+        print('The plugin ', name, ' cannot handlle the following command: ', cmd[0])
 
 if __name__ == "__main__":
     main()
