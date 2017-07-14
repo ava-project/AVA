fc0f77531b9727dd1f1afbc50857b06808f01956
Tommy <thomas.milox@epitech.eu>
2017-07-14 17:34:13 +0200

    removed comments

--- ava/text_to_speech/__init__.py
+++ ava/text_to_speech/__init__.py
@@ -1,7 +1,6 @@
+import talkey
 from ..queues import QueueTtS
 from ..components import _BaseComponent
-
-# import talkey
 
 
 class TextToSpeech(_BaseComponent):
@@ -9,10 +8,10 @@ class TextToSpeech(_BaseComponent):
     def __init__(self):
         super().__init__()
         self.queue_tts = QueueTtS()
-        # self.tts = talkey.Talkey(engine_preference=['google', 'espeak'])
+        self.tts = talkey.Talkey(engine_preference=['google', 'espeak'])
 
     def run(self):
         sentence = self.queue_tts.get()
         print('To say out loud : {}'.format(sentence))
-        # self.tts.say(sentence)
+        self.tts.say(sentence)
         self.queue_tts.task_done()
