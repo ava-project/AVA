676dccbb20e7bba43c7351cff6e1aa211dd5fd06
Guillaume Briard <guiz.briard@gmail.com>
2017-09-03 23:42:22 +0200

    update client side with server updates

--- ava/speech_to_text/STT_EngineWebsocket.py
+++ ava/speech_to_text/STT_EngineWebsocket.py
@@ -4,16 +4,16 @@ import asyncio
 class STT_Engine_WebSocket():
 
     async def recognize(self, stream, queue_manager):
-        async with websockets.connect('ws://192.168.1.74:8766') as websocket:
+        async with websockets.connect('ws://172.17.0.2:8766') as websocket:
             print("Connecting to AVA Servers with address 172.17.0.2:8766")
             print("Sending file to server..")
             await websocket.send(b''.join(stream))
             print("Receiving sentence..")
             message = await websocket.recv()
-            print (message)
+            print(message)
             try:
-                if message["results"][0]["alternatives"][0]["transcript"] :
-                    queue_manager.queue_command.put(message["results"][0]["alternatives"][0]["transcript"])
+                if message:
+                    queue_manager.queue_command.put(message)
                     queue_manager.queue_input.task_done()
                     queue_manager.queue_tts.put("Okay")
             except:
