6a274335cd306c789ce6b7bebb3ac7491ed10292
Tommy <thomasmilox@gmail.com>
2017-11-02 20:35:45 +0000

    Implemented loading bar waiting until AVA is ready

--- ava/ava.py
+++ ava/ava.py
@@ -1,3 +1,4 @@
+from .state import State
 from .components import ComponentManager
 from .input import Input
 from .no_vocal_test import NoVocalTest
@@ -19,7 +20,6 @@ class AVA(object):
             sys.exit('AVA requires the version (1.0.5) of the Software Development Kit.')
 
     def run(self):
-        print('TEST')
         self.manager.add_component(Input)
         self.manager.add_component(Dispatcher)
         self.manager.add_component(NoVocalTest)
@@ -32,17 +32,42 @@ class AVA(object):
         self.manager.add_component(PluginInvoker)
         self.manager.add_component(PluginListener)
         self.manager.start_all()
-        print('AFTER START ALL')
+        State().loading_done()
+        self.manager.ready()
         self.manager.join_all()
 
     def stop(self):
         print('Exiting AVA')
         self.manager.stop_all()
 
+def loading():
+    """
+    """
+    import os
+    import sys
+    from time import sleep
+    sleep(0.25)
+    plugins_nbr = 0
+    path = os.path.join(os.path.expanduser('~'), '.ava', 'plugins')
+    for element in os.listdir(path):
+        if os.path.isdir(os.path.join(path, element)):
+            plugins_nbr += 1
+    n = plugins_nbr * 6
+    for i in range(n):
+        sys.stdout.write('\r')
+        j = (i + 1) / n
+        if not State().is_loading() or j > 1:
+            break
+        sys.stdout.write("Loading plugins [%-20s] %d%%" % ('='*int(20*j), 100*j))
+        sys.stdout.flush()
+        sleep(n / 100 * (plugins_nbr / 3))
+
 def main():
     ava = AVA()
     try:
-        ava.run()
+        import threading
+        threading.Thread(target=ava.run).start()
+        threading.Thread(target=loading).start()
     except Exception as err:
         print(str(err))
     except KeyboardInterrupt as err:
