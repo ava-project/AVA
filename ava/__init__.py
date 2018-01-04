2fe742593ea8dc36257daa95b9775d19ad7a0ddd
Jean-Baptiste Grégoire <j.baptiste159@gmail.com>
2018-01-04 13:07:53 +0100

    Add import to fix dependency for the installer

--- ava/__init__.py
+++ ava/__init__.py
@@ -3,10 +3,10 @@ import sys
 import platform
 import multiprocessing
 
-# if platform.system() == 'Windows':
-#     sys.executable = os.path.join(sys.exec_prefix, 'pythonw.exe')
-# elif platform.system() == 'Darwin':
-#     sys.executable = os.path.join(sys.exec_prefix, 'Python')
-# multiprocessing.freeze_support()
+if platform.system() == 'Windows':
+    sys.executable = os.path.join(sys.exec_prefix, 'pythonw.exe')
+elif platform.system() == 'Darwin':
+    sys.executable = '/usr/bin/pythonw'
+multiprocessing.freeze_support()
 
 __version__ = '0.0.1'
